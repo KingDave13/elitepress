f988176e439959cd9c753b8a8516aedbe6d1201f
David Okaliwe <okaliwedavid@gmail.com>
2024-01-16 20:09:17 +0100

    Updates

---
+++ src/scenes/HomePage.jsx
@@ -0,0 +1,15 @@
+import {
+    Navbar,
+} from '../components';
+
+const HomePage = () => {
+    return (
+        <div>
+            <div>
+                <Navbar />
+            </div>
+        </div>
+    )
+};
+
+export default HomePage;
