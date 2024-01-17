07cf8406c40d97c16a1979e624157d7e9d81a8d4
David Okaliwe <okaliwedavid@gmail.com>
2024-01-17 13:23:33 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './scenes';
-import { Navbar } from './components';
 
 const App = () => {
   return (
-    // <BrowserRouter>
-    //   <Routes >
-    //     <Route path='/' element={<HomePage />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <Routes >
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 };
 
