b64e335a5cfd3dfa3f917acb9d2e771414011af6
David Okaliwe <okaliwedavid@gmail.com>
2024-02-23 10:39:14 +0100

    Updates

--- src/components/EjhVol2Iss1Pubs.jsx
+++ src/components/EjhVol2Iss1Pubs.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from 'react';
+
+import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+
 import { SectionWrapper } from "../hoc";
 import { motion } from 'framer-motion';
 import { fadeIn, textVariant } from '../utils/motion';
@@ -84,6 +88,29 @@ const Publication = ({ pages, abstract, title, authors, route }) => {
 }
 
 const EjhVol2Iss1Pubs = () => {
+    const isMobile = window.innerWidth <= 620;
+
+    const itemsPerPage = 3;
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalItems = Ejhvol2iss1.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const handleNextPage = () => {
+        setCurrentPage(prevPage => prevPage < totalPages ? prevPage + 1 : prevPage);
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage(prevPage => prevPage > 1 ? prevPage - 1 : prevPage);
+    };
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [currentPage]);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
+    const currentItems = Ejhvol2iss1.slice(startIndex, endIndex);
+
     return (
         <section className="w-full md:min-h-[550px] ss:min-h-[300px] 
         min-h-[500px] flex flex-col"
@@ -94,13 +121,54 @@ const EjhVol2Iss1Pubs = () => {
                 justify-center w-full'>
                     <div className='w-full flex flex-col md:gap-12 
                     ss:gap-10 gap-10'>
-                        {Ejhvol2iss1.map((item, index) => (
+                        {currentItems.map((item, index) => (
                             <Publication
                                 key={item.title}
-                                index={index}
+                                index={startIndex + index + 1}
                                 {...item}
                             />
                         ))}
+                    </div>
+
+                    <div className="w-full flex justify-center mt-8
+                    items-center relative">
+                        <div className='flex gap-5'>
+                            <button className='bg-main text-white font-medium
+                            flex items-center justify-center gap-2 md:px-5 
+                            ss:px-5 px-3 md:py-2.5 ss:py-2.5 py-2
+                            rounded-md cursor-pointer grow4 text-[13px]'
+                            onClick={handlePrevPage} 
+                            disabled={currentPage === 1}
+                            >
+                                <GoArrowLeft 
+                                size={isMobile ? 23 : 25}
+                                />
+                            </button>
+                            
+                            <button className='bg-main text-white font-medium
+                            flex items-center justify-center gap-2 md:px-5 
+                            ss:px-5 px-3 md:py-2.5 ss:py-2.5 py-2
+                            rounded-md cursor-pointer grow4 text-[13px]'
+                            onClick={handleNextPage}
+                            disabled={currentPage === totalPages}
+                            >   
+                                <span className={isMobile ? 'hidden' : 'block'}>Next Page</span>
+                                <GoArrowRight 
+                                size={isMobile ? 23 : 20}
+                                />
+                            </button>
+                        </div>
+                        
+                        <div className='flex absolute right-0'>
+                            <p className='text-maintext font-medium 
+                            md:text-[17px] ss:text-[16px] text-[14px]'>
+                                Page <span className='font-bold'>
+                                    {currentPage}
+                                    </span> of <span className='font-bold'>
+                                    {totalPages}
+                                    </span>
+                            </p>
+                        </div>
                     </div>
                 </motion.div>
             </div>
