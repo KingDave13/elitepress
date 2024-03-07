892bbb208123ed70c3932a8aa2f69ded299a2307
David Okaliwe <okaliwedavid@gmail.com>
2024-03-07 22:21:43 +0100

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1459,8 +1459,8 @@ export const Ejhvol2iss2 = [
         pages: "20-33",
     },
     {
-        title: "The Role of Platelets in Host Defence Against Pathogens",
-        authors: "Festus Uchechukwu Onuigwe, Nasiru Maniru, Nkechi Judith Uchechukwu and Emmanuel Ifeanyi Obeagu",
+        title: "Anaemia and Plasmodiasis among children below five years attending OPD at Ishaka Adventist Hospital, Bushenyi District",
+        authors: "Anslem O. Ajugwo, Yakubu Sunday Bot, Emmanuel Ifeanyi Obeagu and Moses Mugabiza",
         abstract: "Platelets are a type of inflammatory cell. As a result, they exhibit characteristics similar to those of a traditional cell-mediated immune effector cell. Platelets are known to contain and can be stimulated to release a variety of bioactive molecules.",
         route: EJHVOL2ISS202,
         pages: "34-45",
