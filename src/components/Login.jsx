b13d1a1371b9e9007b87942deaddcf9deb2f3afc
David Okaliwe <okaliwedavid@gmail.com>
2024-04-09 03:46:54 +0100

    Updates

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -78,27 +78,27 @@ const Login = () => {
       }),
 
       onSubmit: async (values) => {
-        try {
-          const res = await fetch("http://localhost:3001/auth/login", {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              email: values.email,
-              password: values.password,
-            }),
-          });
-          if (res.ok) {
-            console.log('Admin user registered successfully');
-          } else if (res.status === 400) {
-            console.error('An admin with this email already exists');
-          } else {
-            console.error('Failed to register admin user');
-          }
-        } catch (error) {
-          console.error('Error registering admin user:', error);
-        }
+        // try {
+        //   const res = await fetch("http://localhost:3001/auth/login", {
+        //     method: 'POST',
+        //     headers: {
+        //       'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //       email: values.email,
+        //       password: values.password,
+        //     }),
+        //   });
+        //   if (res.ok) {
+        //     console.log('Admin user registered successfully');
+        //   } else if (res.status === 400) {
+        //     console.error('An admin with this email already exists');
+        //   } else {
+        //     console.error('Failed to register admin user');
+        //   }
+        // } catch (error) {
+        //   console.error('Error registering admin user:', error);
+        // }
       },
   });
 
