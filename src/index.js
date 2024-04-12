045f250c7b7711faae9b568d42b72acfedce08cf
David Okaliwe <okaliwedavid@gmail.com>
2024-04-12 23:42:53 +0100

    Updates

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import authReducer from './state';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session'
 import { PersistGate } from 'redux-persist/integration/react';
 
-const persistConfig = { key: 'root', storage, version: 1 };
+const persistConfig = { key: 'root', storage: storageSession, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
@@ -24,7 +24,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
