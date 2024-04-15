79440be7aaf44b68c3e372d8e831f0157800a7dc
David Okaliwe <okaliwedavid@gmail.com>
2024-04-15 14:48:44 +0100

    Updates

--- src/components/NavbarAdminLogin.jsx
+++ src/components/NavbarAdminLogin.jsx
@@ -9,33 +9,33 @@ const NavbarAdminLogin = () => {
     <nav className={`${styles.paddingX} w-full flex items-center fixed 
     md:py-6 ss:py-6 py-5 top-0 z-20 bg-primaryalt`}
     >
-        <div className="w-full flex items-center max-w-[82rem] mx-auto">
-            <div className='flex w-full justify-between items-center'>
-                <Link to='/admin'
-                onClick={() => {
-                window.scrollTo(0, 0);
-                }}
-                className='md:mr-10 ss:mr-8 mr-6'>
-                <img src={logo} alt='logo'
-                className='md:w-[75px] ss:w-[75px] w-[70px] h-auto'/>
-                </Link>
+      <div className="w-full flex items-center max-w-[82rem] mx-auto">
+        <div className='flex w-full justify-between items-center'>
+          <Link to='/admin'
+          onClick={() => {
+          window.scrollTo(0, 0);
+          }}
+          className='md:mr-10 ss:mr-8 mr-6'>
+          <img src={logo} alt='logo'
+          className='md:w-[75px] ss:w-[75px] w-[60px] h-auto'/>
+          </Link>
 
-                <div className="flex w-full flex-col">
-                    <h1 className='text-main font-bold md:text-[30px]
-                    ss:text-[28px] text-[21px] md:leading-[40px] 
-                    ss:leading-[40px] leading-[30px]'>
-                    Admin Dashboard
-                    </h1>
+          <div className="flex w-full flex-col">
+            <h1 className='text-main font-bold md:text-[30px]
+            ss:text-[28px] text-[20px] md:leading-[40px] 
+            ss:leading-[40px] leading-[30px]'>
+            Admin Dashboard
+            </h1>
 
-                    <p className='text-maintext md:text-[17px]
-                    ss:text-[16px] text-[13px] md:leading-[25px] 
-                    ss:leading-[25px] leading-[15px]'>
-                    Manage papers and articles for each journal, 
-                    submisson requests and more.
-                    </p>
-                </div>
-            </div>
+            <p className='text-maintext md:text-[17px]
+            ss:text-[16px] text-[13px] md:leading-[25px] 
+            ss:leading-[25px] leading-[15px]'>
+            Manage papers and articles for each journal, 
+            submisson requests and more.
+            </p>
+          </div>
         </div>
+      </div>
     </nav>
   );
 };
