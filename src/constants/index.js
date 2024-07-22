052dc4632cb19f067bf2a0edf4feb2731755cd42
David Okaliwe <okaliwedavid@gmail.com>
2024-07-22 17:23:16 +0100

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1029,8 +1029,8 @@ export const ejnhsSideLinks = [
         title: "Current Issue",
         links: [
             {
-                name: "Volume 2, Issue 6. (2024)",
-                route: "/journals/EJNHS/VOL2ISS6",
+                name: "Volume 2, Issue 7. (2024)",
+                route: "/journals/EJNHS/VOL2ISS7",
             },
         ],
     },
